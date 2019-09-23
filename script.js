7691ff8166a987546a203016ac692f951509eade
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2019-09-22 19:20:14 -0500

    found the bug

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const secondHand = document.querySelector("[data-second-hand]");
 function setClock() {
   const currentDate = new Date();
   const secondsRatio = currentDate.getSeconds() / 60;
-  const minutesRatio = (secondsRatios + currentDate.getMinutes()) / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
   const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
   setRotation(secondHand, secondsRatio);
-  setRotation(secondHand, minutesRatio);
-  setRotation(secondHand, hoursRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
 }
 
 function setRotation(element, rotationRatio) {
